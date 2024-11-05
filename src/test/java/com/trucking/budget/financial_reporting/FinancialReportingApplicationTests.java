b75f729518a6b36e9bb7e0dda1ae59ef0446e74f
tripathiramnarayan <tripathiramnarayan5@gmail.com>
2024-11-05 22:48:24 +0530

    added financial reporting feature 1

---
+++ src/test/java/com/trucking/budget/financial_reporting/FinancialReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trucking.budget.financial_reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
